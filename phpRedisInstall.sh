02ab462cd3b1ca5c9e0b901b77af5c9bf9c345ef
marhode <maximilian.rhode@capgemini.com>
2024-05-08 13:47:03 +0200

    Bugfixed redis path

--- phpRedisInstall.sh
+++ phpRedisInstall.sh
@@ -2,7 +2,7 @@
 curl https://pecl.php.net/get/redis-6.0.2.tgz --output /tmp/redis-6.0.2.tgz
 tar xzf /tmp/redis-6.0.2.tgz
 rm /tmp/redis-6.0.2.tgz
-cd /tmp/redis-6.0.2
+cd /redis-6.0.2
 phpize
 ./configure
 make
