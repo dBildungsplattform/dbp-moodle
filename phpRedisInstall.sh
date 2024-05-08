07e3cb383046c30b1194da3cee4ebf2a120bf422
marhode <maximilian.rhode@capgemini.com>
2024-05-08 13:40:33 +0200

    changed download path to be BSI conform

--- phpRedisInstall.sh
+++ phpRedisInstall.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-curl https://pecl.php.net/get/redis-6.0.2.tgz --output /redis-6.0.2.tgz
-tar xzf /redis-6.0.2.tgz
-rm /redis-6.0.2.tgz
-cd /redis-6.0.2
+curl https://pecl.php.net/get/redis-6.0.2.tgz --output /tmp/redis-6.0.2.tgz
+tar xzf /tmp/redis-6.0.2.tgz
+rm /tmp/redis-6.0.2.tgz
+cd /tmp/redis-6.0.2
 phpize
 ./configure
 make
