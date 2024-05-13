6326b1278d78e1bfd4e80e1aa358528efb1b1835
marhode <maximilian.rhode@capgemini.com>
2024-05-13 08:42:52 +0200

    Run whole php redis install script from tmp folder

--- phpRedisInstall.sh
+++ phpRedisInstall.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 curl https://pecl.php.net/get/redis-6.0.2.tgz --output /tmp/redis-6.0.2.tgz
+cd /tmp
 tar xzf /tmp/redis-6.0.2.tgz
 rm /tmp/redis-6.0.2.tgz
-cd /redis-6.0.2
+cd /tmp/redis-6.0.2
 phpize
 ./configure
 make
