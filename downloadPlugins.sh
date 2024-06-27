b2852dd2247772d13ac1bdc2d2026f5a30ce10e5
marhode <maximilian.rhode@capgemini.com>
2024-06-27 11:09:28 +0200

    Added missing moosh plugin-list

--- downloadPlugins.sh
+++ downloadPlugins.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd /plugins
+php /moosh/moosh.php plugin-list
 php /moosh/moosh.php plugin-download -v 4.1 mod_hvp
 php /moosh/moosh.php plugin-download -v 4.1 mod_booking
 php /moosh/moosh.php plugin-download -v 4.1 tool_certificate
