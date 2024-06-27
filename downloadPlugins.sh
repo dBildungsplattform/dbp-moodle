63a4650ab2df4315c731e9ad32e2fba80b5b32d3
marhode <maximilian.rhode@capgemini.com>
2024-06-27 11:49:01 +0200

    Changed plugin versions for testing

--- downloadPlugins.sh
+++ downloadPlugins.sh
@@ -2,5 +2,5 @@
 cd /plugins
 php /moosh/moosh.php plugin-list
 php /moosh/moosh.php plugin-download -v 4.1 mod_hvp
-php /moosh/moosh.php plugin-download -v 4.1 mod_booking
+php /moosh/moosh.php plugin-download -v 4.2 mod_booking
 php /moosh/moosh.php plugin-download -v 4.1 tool_certificate
