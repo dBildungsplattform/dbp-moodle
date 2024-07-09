79dfb6abd1132d66abda9661793ce69b4416d943
Maximilian Greve <maximilian.greve@capgemini.com>
2024-07-09 08:55:17 +0000

    add plugins to downloads

--- downloadPlugins.sh
+++ downloadPlugins.sh
@@ -1,7 +1,30 @@
 #!/bin/bash
 cd /plugins
 php /moosh/moosh.php plugin-list
-php /moosh/moosh.php plugin-download -v 4.1 mod_hvp
-php /moosh/moosh.php plugin-download -v 4.2 mod_booking
+
+# Dependencies
 php /moosh/moosh.php plugin-download -v 4.1 local_wunderbyte_table # Dependency of mod_booking
+
+# Plugins
+php /moosh/moosh.php plugin-download -v 4.1 mod_etherpadlite
+php /moosh/moosh.php plugin-download -v 4.1 mod_hvp
+php /moosh/moosh.php plugin-download -v 4.1 mod_groupselect
+php /moosh/moosh.php plugin-download -v 4.1 mod_jitsi
+php /moosh/moosh.php plugin-download -v 4.1 mod_pdfannotator
+php /moosh/moosh.php plugin-download -v 4.1 mod_skype
+php /moosh/moosh.php plugin-download -v 4.1 mod_zoom
+php /moosh/moosh.php plugin-download -v 4.1 mod_booking
+php /moosh/moosh.php plugin-download -v 4.1 mod_reengagement
+php /moosh/moosh.php plugin-download -v 4.1 mod_unilabel
+php /moosh/moosh.php plugin-download -v 4.1 mod_geogebra
+php /moosh/moosh.php plugin-download -v 4.1 format_remuiformat
+php /moosh/moosh.php plugin-download -v 4.1 format_tiles
+php /moosh/moosh.php plugin-download -v 4.1 format_topcoll
+php /moosh/moosh.php plugin-download -v 4.1 auth_oidc
+php /moosh/moosh.php plugin-download -v 4.1 auth_saml2
+php /moosh/moosh.php plugin-download -v 4.1 block_dash
+php /moosh/moosh.php plugin-download -v 4.1 block_sharing_cart
+php /moosh/moosh.php plugin-download -v 4.1 block_xp
 php /moosh/moosh.php plugin-download -v 4.1 tool_certificate
+php /moosh/moosh.php plugin-download -v 4.1 mod_coursecertificate
+php /moosh/moosh.php plugin-download -v 4.1 theme_boost_union
