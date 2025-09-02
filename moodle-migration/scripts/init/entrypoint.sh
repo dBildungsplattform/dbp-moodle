e266f57df85d62e844503b70c8d320d5db03e86f
Nils Braehmer <nils.braehmer@capgemini.com>
2025-09-02 12:20:19 +0000

    Add entrypoint

--- moodle-migration/scripts/init/entrypoint.sh
+++ moodle-migration/scripts/init/entrypoint.sh
@@ -66,9 +66,9 @@ upgrade_if_pending() {
 }
 
 startDbpMoodleSetup() {
-    info "Starting dbp Moodle setup"
-    /scripts/init/apache/apacheSetup.sh
-    /scripts/init/php/phpSetup.sh
+    info "Starting dbp Moodle (Postgres) setup"
+    # /scripts/init/apache/apacheSetup.sh
+    # /scripts/init/php/phpSetup.sh
     /scripts/init/postgres/postgresSetup.sh
     MODULE=dbp info "Initial Moodle setup finished"
 }
