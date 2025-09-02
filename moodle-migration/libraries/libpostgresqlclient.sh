b0c0129c0c529520b7923d6c498b6f6a45b407e8
Nils Braehmer <nils.braehmer@capgemini.com>
2025-09-02 11:14:35 +0000

    Testing postgres client implementation

--- moodle-migration/libraries/libpostgresqlclient.sh
+++ moodle-migration/libraries/libpostgresqlclient.sh
@@ -1,1 +1,269 @@
 # https://github.com/bitnami/containers/blob/main/bitnami/moodle/5.0/debian-12/rootfs/opt/bitnami/scripts/libpostgresqlclient.sh
+
+########################
+# Perform actions to a database
+# Globals:
+#   POSTGRESQL_CLIENT_*
+# Arguments:
+#   None
+# Returns:
+#   None
+#########################
+postgresql_client_initialize() {
+    local -a database_names
+    read -r -a database_names <<< "$(tr ',;' ' ' <<< "$POSTGRESQL_CLIENT_CREATE_DATABASE_NAMES")"
+    # Wait for the database to be accessible if any action needs to be performed
+    if [[ -n "$POSTGRESQL_CLIENT_CREATE_DATABASE_USERNAME" || "${#database_names[@]}" -gt 0 ]]; then
+        info "Trying to connect to the database server"
+        check_postgresql_connection() {
+            echo "SELECT 1" | postgresql_remote_execute "$POSTGRESQL_CLIENT_DATABASE_HOST" "$POSTGRESQL_CLIENT_DATABASE_PORT_NUMBER" "postgres" "$POSTGRESQL_CLIENT_POSTGRES_USER" "$POSTGRESQL_CLIENT_POSTGRES_PASSWORD"
+        }
+        if ! retry_while "check_postgresql_connection"; then
+            error "Could not connect to the database server"
+            return 1
+        fi
+    fi
+    # Ensure a database user exists in the server
+    if [[ -n "$POSTGRESQL_CLIENT_CREATE_DATABASE_USERNAME" ]]; then
+        info "Creating database user ${POSTGRESQL_CLIENT_CREATE_DATABASE_USERNAME}"
+        local -a args=("$POSTGRESQL_CLIENT_CREATE_DATABASE_USERNAME" "--host" "$POSTGRESQL_CLIENT_DATABASE_HOST" "--port" "$POSTGRESQL_CLIENT_DATABASE_PORT_NUMBER")
+        [[ -n "$POSTGRESQL_CLIENT_CREATE_DATABASE_PASSWORD" ]] && args+=("-p" "$POSTGRESQL_CLIENT_CREATE_DATABASE_PASSWORD")
+        postgresql_ensure_user_exists "${args[@]}"
+    fi
+    # Ensure a database exists in the server (and that the user has write privileges, if specified)
+    if [[ "${#database_names[@]}" -gt 0 ]]; then
+        local -a createdb_args extensions
+        read -r -a extensions <<< "$(tr ',;' ' ' <<< "$POSTGRESQL_CLIENT_CREATE_DATABASE_EXTENSIONS")"
+        for database_name in "${database_names[@]}"; do
+            info "Creating database ${database_name}"
+            createdb_args=("$database_name" "--host" "$POSTGRESQL_CLIENT_DATABASE_HOST" "--port" "$POSTGRESQL_CLIENT_DATABASE_PORT_NUMBER")
+            [[ -n "$POSTGRESQL_CLIENT_CREATE_DATABASE_USERNAME" ]] && createdb_args+=("-u" "$POSTGRESQL_CLIENT_CREATE_DATABASE_USERNAME")
+            postgresql_ensure_database_exists "${createdb_args[@]}"
+            # TODO I think this not used by us and can be removed?  Ensure the list of extensions are enabled in the specified database
+            if [[ "${#extensions[@]}" -gt 0 ]]; then
+                for extension_to_create in "${extensions[@]}"; do
+                    echo "CREATE EXTENSION IF NOT EXISTS ${extension_to_create}" | postgresql_remote_execute "$POSTGRESQL_CLIENT_DATABASE_HOST" "$POSTGRESQL_CLIENT_DATABASE_PORT_NUMBER" "$database_name" "$POSTGRESQL_CLIENT_POSTGRES_USER" "$POSTGRESQL_CLIENT_POSTGRES_PASSWORD"
+                done
+            fi
+        done
+    fi
+    # Avoid exit code of previous commands to affect the result of this function
+    true
+}
+
+########################
+# Execute an arbitrary query/queries against a remote PostgreSQL service
+# Stdin:
+#   Query/queries to execute
+# Globals:
+#   DB_*
+# Arguments:
+#   $1 - Remote PostgreSQL service hostname
+#   $2 - Remote PostgreSQL service port
+#   $3 - Database where to run the queries
+#   $4 - User to run queries
+#   $5 - Password
+#   $6 - Extra options (eg. -tA)
+# Returns:
+#   None
+postgresql_remote_execute() {
+    "postgresql_remote_execute_print_output" "$@" >/dev/null 2>&1
+}
+
+########################
+# Execute an arbitrary query/queries against a remote PostgreSQL service and print to stdout
+# Stdin:
+#   Query/queries to execute
+# Globals:
+#   DB_*
+# Arguments:
+#   $1 - Remote PostgreSQL service hostname
+#   $2 - Remote PostgreSQL service port
+#   $3 - Database where to run the queries
+#   $4 - User to run queries
+#   $5 - Password
+#   $6 - Extra options (eg. -tA)
+# Returns:
+#   None
+postgresql_remote_execute_print_output() {
+    local -r hostname="${1:?hostname is required}"
+    local -r port="${2:?port is required}"
+    local -a args=("-h" "$hostname" "-p" "$port")
+    shift 2
+    "postgresql_execute_print_output" "$@" "${args[@]}"
+}
+
+########################
+# Execute an arbitrary query/queries against the running PostgreSQL service and print the output
+# Stdin:
+#   Query/queries to execute
+# Globals:
+#   POSTGRESQL_*
+# Arguments:
+#   $1 - Database where to run the queries
+#   $2 - User to run queries
+#   $3 - Password
+#   $4 - Extra options (eg. -tA)
+# Returns:
+#   None
+#########################
+postgresql_execute_print_output() {
+    local -r db="${1:-}"
+    local -r user="${2:-postgres}"
+    local -r pass="${3:-}"
+    local opts
+    read -r -a opts <<<"${@:4}"
+
+    local args=("-U" "$user" "-p" "${POSTGRESQL_PORT_NUMBER:-5432}" "-h" "127.0.0.1")
+    [[ -n "$db" ]] && args+=("-d" "$db")
+    [[ "${#opts[@]}" -gt 0 ]] && args+=("${opts[@]}")
+
+    # Execute the Query/queries from stdin
+    PGPASSWORD=$pass psql "${args[@]}"
+}
+
+# from libos.sh
+########################
+# Retries a command a given number of times
+# Arguments:
+#   $1 - cmd (as a string)
+#   $2 - max retries. Default: 12
+#   $3 - sleep between retries (in seconds). Default: 5
+# Returns:
+#   Boolean
+#########################
+retry_while() {
+    local cmd="${1:?cmd is missing}"
+    local retries="${2:-12}"
+    local sleep_time="${3:-5}"
+    local return_value=1
+
+    read -r -a command <<<"$cmd"
+    for ((i = 1; i <= retries; i += 1)); do
+        "${command[@]}" && return_value=0 && break
+        sleep "$sleep_time"
+    done
+    return $return_value
+}
+
+########################
+# Optionally create the given database user
+# Flags:
+#   -p|--password - database password
+#   --host - database host
+#   --port - database port
+# Arguments:
+#   $1 - user
+# Returns:
+#   None
+#########################
+postgresql_ensure_user_exists() {
+    local -r user="${1:?user is missing}"
+    local password=""
+    # For accessing an external database
+    local db_host=""
+    local db_port=""
+
+    # Validate arguments
+    shift 1
+    while [ "$#" -gt 0 ]; do
+        case "$1" in
+        -p | --password)
+            shift
+            password="${1:?missing password}"
+            ;;
+        --host)
+            shift
+            db_host="${1:?missing database host}"
+            ;;
+        --port)
+            shift
+            db_port="${1:?missing database port}"
+            ;;
+        *)
+            echo "Invalid command line flag $1" >&2
+            return 1
+            ;;
+        esac
+        shift
+    done
+
+    local -a postgresql_execute_cmd=("postgresql_execute")
+    [[ -n "$db_host" && -n "$db_port" ]] && postgresql_execute_cmd=("postgresql_remote_execute" "$db_host" "$db_port")
+    # TODO I think this can be simplified to "$POSTGRESQL_CLIENT_POSTGRES_USER" "$POSTGRESQL_CLIENT_POSTGRES_PASSWORD"
+    # local -a postgresql_execute_flags=("postgres" "$(get_env_var_value POSTGRES_USER)" "$(get_env_var_value POSTGRES_PASSWORD)")
+    local -a postgresql_execute_flags=("postgres" "$POSTGRESQL_CLIENT_POSTGRES_USER" "$POSTGRESQL_CLIENT_POSTGRES_PASSWORD")
+
+    "${postgresql_execute_cmd[@]}" "${postgresql_execute_flags[@]}" <<EOF
+DO
+\$do\$
+BEGIN
+   IF NOT EXISTS (
+      SELECT FROM pg_catalog.pg_roles WHERE rolname = '${user}'
+   ) THEN
+      CREATE ROLE "${user}" LOGIN PASSWORD '${password}';
+   END IF;
+END
+\$do\$;
+EOF
+}
+
+########################
+# Optionally create the given database, and then optionally give a user
+# full privileges on the database.
+# Flags:
+#   -u|--user - database user
+#   --host - database host
+#   --port - database port
+# Arguments:
+#   $1 - database name
+# Returns:
+#   None
+#########################
+postgresql_ensure_database_exists() {
+    local -r database="${1:?database is missing}"
+    local user=""
+    # For accessing an external database
+    local db_host=""
+    local db_port=""
+
+    # Validate arguments
+    shift 1
+    while [ "$#" -gt 0 ]; do
+        case "$1" in
+        -u | --user)
+            shift
+            user="${1:?missing database user}"
+            ;;
+        --host)
+            shift
+            db_host="${1:?missing database host}"
+            ;;
+        --port)
+            shift
+            db_port="${1:?missing database port}"
+            ;;
+        *)
+            echo "Invalid command line flag $1" >&2
+            return 1
+            ;;
+        esac
+        shift
+    done
+
+    local -a postgresql_execute_cmd=("postgresql_execute")
+    [[ -n "$db_host" && -n "$db_port" ]] && postgresql_execute_cmd=("postgresql_remote_execute" "$db_host" "$db_port")
+    # local -a postgresql_execute_flags=("postgres" "$(get_env_var_value POSTGRES_USER)" "$(get_env_var_value POSTGRES_PASSWORD)")
+    local -a postgresql_execute_flags=("postgres" "$POSTGRESQL_CLIENT_POSTGRES_USER" "$POSTGRESQL_CLIENT_POSTGRES_PASSWORD")
+
+    "${postgresql_execute_cmd[@]}" "${postgresql_execute_flags[@]}" <<EOF
+SELECT 'CREATE DATABASE "${database}"'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = '${database}')\gexec
+EOF
+    if [[ -n "$user" ]]; then
+        local -a grant_flags=("$user" "$database")
+        [[ -n "$db_host" ]] && grant_flags+=("$db_host")
+        [[ -n "$db_port" ]] && grant_flags+=("$db_port")
+        postgresql_ensure_user_has_database_privileges "${grant_flags[@]}"
+    fi
+}
--- moodle-migration/scripts/init/postgres/postgresSetup.sh
+++ moodle-migration/scripts/init/postgres/postgresSetup.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
+
 # https://github.com/bitnami/containers/blob/main/bitnami/moodle/5.0/debian-12/rootfs/opt/bitnami/scripts/postgresql-client/setup.sh
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+. /scripts/libpostgresqlclient.sh
+
+# Load PostgreSQL Client environment variables
+. /scripts/init/postgres/postgres-client-env.sh
+
+# Execute the main function:
+postgresql_client_initialize
