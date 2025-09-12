cb8b84f7b1b6c8624ecbbedb3bec51ec0fab85e0
marhode <121863882+marhode@users.noreply.github.com>
2025-09-12 13:15:23 +0200

    Dbp 1620 moodle migration php setup (#113)
    * Added bitnami functions and adjusted logic for the php setup and configuration
    
    * Added sleep for debugging purposes
    
    * Adjusted way of loading php configuration with php.ini
    
    * Adjusted php for upcoming webserver implementation in the entrypoint
    
    * Test for solving the last failed checks
    
    * Changed pipeline for migration image

--- moodle-migration/scripts/init/php/phpSetup.sh
+++ moodle-migration/scripts/init/php/phpSetup.sh
@@ -1,1 +1,36 @@
-# https://github.com/bitnami/containers/blob/main/bitnami/moodle/5.0/debian-12/rootfs/opt/bitnami/scripts/php/setup.sh
+#!/bin/bash
+# Copyright Broadcom, Inc. All Rights Reserved.
+# SPDX-License-Identifier: APACHE-2.0
+
+# shellcheck disable=SC1091
+
+set -o errexit
+set -o nounset
+set -o pipefail
+# set -o xtrace # Uncomment this line for debugging purposes
+
+# Load libraries
+. /scripts/libphp.sh
+. /scripts/libos.sh
+. /scripts/liblog.sh
+
+# Load PHP-FPM environment variables
+. /scripts/init/php/php-env.sh
+
+error_code=0
+
+php_initialize
+
+if is_php_fpm_not_running; then # adjust pid path
+    exec php-fpm -F --pid $PHP_FPM_PID_FILE -y $PHP_FPM_CONF_FILE >/dev/null 2>&1 &
+    if ! retry_while "is_php_fpm_running"; then
+        error "php-fpm did not start"
+        error_code=1
+    else
+        info "php-fpm started"
+    fi
+else
+    info "php-fpm is already running"
+fi
+
+exit "$error_code"
