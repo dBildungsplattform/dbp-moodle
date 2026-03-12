e32cb6a4cdbb85bf86ab35f56fba06fb7eb38aea
Daniel Beckmann <daniel.beckmann@dataport.de>
2026-03-12 14:33:47 +0000

    fix

--- moodle/scripts/install/downloadPlugins.sh
+++ moodle/scripts/install/downloadPlugins.sh
@@ -74,7 +74,7 @@ download_oidc
 moosh plugin-list > /dev/null
 set -x
 for plugin in "${moodle_plugin_list[@]}"; do
-    if (( $plugin_index > 0 && $plugin_index % 15 == 0 )); then
+    if (( $plugin_index > 0 && $plugin_index % 5 == 0 )); then
         echo "Reached batch of 15 plugins. Sleeping for 60 seconds..."
         sleep 60
     fi
